3e0911ee4a61c28cc74fdb5baddb3cb0104c3fe8
UserAnonn <connieansaldi5@gmail.com>
2023-01-03 21:14:19 -0300

    netflix template

---
+++ netflix.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Netflix </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <a href="https://cdnlogo.com/logo/netflix_11770.html"><img src="https://cdn.cdnlogo.com/logos/n/75/netflix.svg" height="50" class="mt-3 ms-3"></a>
+        <p class="fs-1 ms-3 mt-3 fw-bold">Un nuevo dispositivo está usando tu cuenta</p>
+        <p class="fs-5 ms-3 mt-3">Estimado usuario: </p>
+        <p class="fs-5 ms-3 mt-3">Se inició sesión en su cuenta de Netflix desde un nuevo dispositivo.</p>
+    <div class="container sm ms-3 ps-4 border p-3">
+        <p class="fs-5 fw-bold text-sm-start">Información detallada </p>
+        <p class="fs-6 fw-bold text-sm-start">Dispositivo </p>
+        <p class="fs-6 text-sm-start">Teléfono Android </p>
+        <p class="fs-6 fw-bold text-sm-start">Ubicación </p>
+        <p class="fs-6 text-sm-start">Provincia de Buenos Aires, Argentina(Es posible que la ubicación no sea exacta). </p>
+        <p class="fs-6 fw-bold text-sm-start">Fecha y hora </p>
+        <p class="fs-6 text-sm-start">31 de enero, 1:34 GMT-3 </p>
+    </div>
+    <div class="ms-3 mt-3">
+        <p class="fs-6 fw-bold text-sm-start">Si fue usted o una persona de su hogar: </p>
+        <p class="fs-6 text-sm-start">¡Hora de disfrutar la próxima historia! ¿Qué tal esta? <a href="https://wwww.netflix.com" class="text-decoration-none">Películas </a> </p>
+        <p class="fs-6 fw-bold text-sm-start">Si otra persona inició sesión con tu cuenta: </p>
+        <p class="fs-6 text-sm-start">Recuerde que solo las personas que sean miembros de su hogar pueden usar su cuenta.  </p>
+        <p class="fs-6 fw-bold text-sm-start">Si no sabe quién fue: </p>
+        <p class="fs-6 text-sm-start">Le recomendamos que <a href="https://wwww.netflix.com" class="text-decoration-none">cambie la contraseña </a> de inmediato para proteger tucuenta.</p>
+        <p class="fs-6 fw-bold text-sm-start">Estamos aquí para asistirte </p>
+        <p class="fs-6 text-sm-start">Visita el <a href="https://wwww.netflix.com" class="text-decoration-none">Centro de ayuda </a> para obtener más información o <a href="https://wwww.netflix.com" class="text-decoration-none">contáctanos. </a></p>
+        <p class="fs-6 fw-bold text-sm-start"><small>El equipo de Netflix </small> </p>
+        <hr class="bg-dark border border-dark ">
+    </div>
+    <div class="container sm ms-3 ps-4 border border-danger mt-4 pt-3 mb-3">
+        <p class="text-center fw-bold"><a href="https://wwww.netflix.com" class="text-danger text-decoration-none">Ver todas las series y películas </a></p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-1">
+                <a href="https://cdnlogo.com/logo/netflix_11770.html"><img src="https://cdn.cdnlogo.com/logos/n/75/netflix.svg" height="50" class="mt-3 ms-3"></a>
+            </div>
+        <div class="col pt-3">
+            ¿Preguntas? Visita el <a href="https://wwww.netflix.com" class="text-decoration-none">Centro de ayuda </a><br>
+            <small>100 Winchester Circle, Los Gatos, CA 95032, EE. UU. </small>
+                <div class="mt-5 mb-5">
+                    <a href="https://wwww.netflix.com" class="text-decoration-none"> Configuración de comunicaciones</a><br>
+                    <a href="https://wwww.netflix.com" class="text-decoration-none"> Términos de uso</a><br>
+                    <a href="https://wwww.netflix.com" class="text-decoration-none"> Privacidad</a><br>
+                    <a href="https://wwww.netflix.com" class="text-decoration-none"> Centro de ayuda</a><br>
+                </div>
+            <div class="mt-3 mb-5">
+                <p mt-3><small>Netflix le envió este mensaje como parte de tu membresía. <br>SRC: 12853_es_AR</small></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
