11f8b250a4b73fecdf75b9cde8e912c051d4a591
UserAnonn <connieansaldi5@gmail.com>
2023-01-04 12:05:06 -0300

    comunicaciones internas

---
+++ comunicaciones.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cencommerce </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <img src="https://i.ibb.co/hgvh7gx/cenco.jpg" class="float-end" alt="..." height="600">
+    <div class="mx-auto d-block">
+        <img src="https://i.ibb.co/BBjJkSM/logo.jpg" class="float-right mt-5 ms-3 mb-3" width="270" height="60" alt="...">
+    </div>
+
+    <div>
+        <img src="https://i.ibb.co/bHZqxKN/heading.jpg" class="float-right mt-5 ms-3" width="400" alt="">
+    </div>
+
+    <div class="float-right mt-3 ms-4">
+        <p class="fs-3 text-primary fw-bold">Encuentro histórico entre líderes de Cencommerce y TI Chile </p>
+    </div>
+
+    <div class="float-right mt-5 ms-4">
+        <p class="fs-4 text-secondary">Nuestro objetivo es trabajar <span class="fw-bold text-primary"> como un solo equipo Tech, alineados y coordinados </span>(rompiendo silos) para mejorar la experiencia onmicanal de nuestros clientes y usuarios, maximizando la entrega de valor a cada negocio.</p>
+    </div>
+
+    <div class="container sm ms-3 ps-4 border p-3">
+        <p class="fs-4 text-secondary">Es por eso que nos reunimos en el auditorio de Florida Center, donde nos 
+            conocimos entre los distintos equipos, compartiendo un exquisito desayuno, en el que 
+        <span class="fw-bold text-primary">
+            ambos equipos presentaron sus estrategias, visiones, roadmaps y áreas de trabajo, intercambiando
+            preguntas y respuestas,
+        </span> y lo más importante, estableciendo un claro networking de aquí hacia adelante para lograr
+       los objetivos planteados. </p>
+
+       <p class="fs-4 text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt facere aspernatur voluptatibus distinctio quia recusandae doloremque eveniet, nam possimus repellat saepe deleniti neque tempore magni? Atque culpa molestiae quam quidem.
+       Commodi, dignissimos labore! In, fugit repellat! Odit autem dicta iste dolores sapiente in deleniti quos alias ex minus quidem, incidunt commodi mollitia voluptas similique adipisci, cupiditate, quae fuga quod iure.
+       Omnis, ea soluta est porro nam labore, minima dicta temporibus ab impedit necessitatibus ipsum doloribus. Magnam tempore rerum officiis iusto neque dolore enim eum suscipit perspiciatis nemo expedita, ea nulla.
+       Impedit optio eos possimus enim minima a doloremque deleniti. Non enim ullam ex, repudiandae a doloribus sapiente dolore natus dignissimos, nesciunt, rerum voluptatem aliquam eveniet dolores sequi cum recusandae! Aperiam.
+       </p>
+       <img src="https://i.ibb.co/ssjqm4W/carr.jpg" class="float-end pe-5 mt-0 mb-4" width="300"  alt="">
+       <img src="https://i.ibb.co/BBjJkSM/logo.jpg" class="float-right mt-5 ms-3 mb-3" width="270" height="60"  alt="">
+    </div>
+</body>
+</html>
