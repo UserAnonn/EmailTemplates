21f914c03fec1a744b3e0bcea526266624e516b0
UserAnonn <connieansaldi5@gmail.com>
2023-01-04 09:15:20 -0300

    seg-inform template

---
+++ seg_inform.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seguridad informática CencoSud</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <p class="mt-4 ms-4">Estimado <span class="fst-italic">Usuario, </span> </p>
+    <p class="mt-4 ms-4">Le informamos que la clave con la que ingresa a su equipo, caducará <span class="fw-bold text-secondary">en 9 días </span>, por lo cual se recomienda cambiarla antes de que esto suceda. </p>
+    <p class="mt-4 ms-4">Recuerde que usted debe realizar el cambio de la siguiente forma: </p>
+    <div class="mt-5">
+        <div class="container col-7">
+            <p class="fs-4 text-secondary fw-bold">INSTRUCCIONES   </p>
+        </div>
+    </div>
+
+    <p class="fs-6 fw-bold mt-0 ms-4 text-primary">Puede hacer el cambio por Reset:</p>
+    <div class="container col-7 ps-0">
+        <button class="btn btn-sm mb-5">
+            <img src="https://i.ibb.co/m00GQkZ/boton.jpg" height="60" width="180"/>
+        </button>
+    </div>
+
+    <div class="mt-5">
+        <hr class="bg-dark border border-dark ">
+    </div>
+
+    <p class="fs-5 text-secondary fw-bold ms-4">Recomendaciones   </p>
+    <ul>
+        <li>
+        Recuerde NO ingresar su usuario/contraseña de Cencosud para suscribirse a sitios externos, ya que estos datos podrían ser usados de manera fraudulenta y comprometer su identidad.
+        </li>
+        <li>
+        Nunca utilice una contraseña derivada de sus datos personales.
+        </li>
+    </ul>
+    
+    <p class="fs-6 text-secondary text center fw-bold ms-4">Cualquier inconveniente no dude en contactarnos a: seguridad.informatica@cencosud.com</p>
+
+    <footer class="fs-5 text-muted ms-2 mt-5 mb-5">
+        <span class="fw-bold">
+        Seguridad Informática
+        </span>
+     | IT Governance & Compliance
+    </footer>
+</body>
+</html>
